24eaa7a4d4a4c345b5aee6dee90cc14de9c3a4d6
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-02-11 22:32:13 +0530

    predict.py : Feed data from CSV file provided

--- predict.py
+++ predict.py
@@ -2,3 +2,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
+
+#Feed the data from csv file
+data_feed=pd.read_csv('data_stocks.csv',usecols = [0,1],skiprows = [0],header=None)
+#print(data_feed) #Print the values(Debugging only)
+
