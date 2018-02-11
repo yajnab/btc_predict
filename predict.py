a8fb0b95b1ea8167fecc535680cc360daac6414d
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2018-02-11 22:32:37 +0530

    predict.py : Fetch values for Graph plotting

--- predict.py
+++ predict.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 data_feed=pd.read_csv('data_stocks.csv',usecols = [0,1],skiprows = [0],header=None)
 #print(data_feed) #Print the values(Debugging only)
 
+#Plotting
+x=data_feed[0] #Date Value
+y=data_feed[1] #Price Value
+
+#plt.plot(x,y) #Plot for Debugging
+#plt.show()  #Plot for Debugging
